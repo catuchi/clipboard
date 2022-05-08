2b2fc7d09dbb30449e633083bb4d2d79a0857489
Derek Harvey <derek.harvey0422@gmail.com>
2022-05-08 19:11:56 +0000

    add more route logic

--- planning/routeLogic.js
+++ planning/routeLogic.js
@@ -78,6 +78,78 @@ app.post("/logout", (req, res) => {
   res.redirect("/");
 })
 
+app.get("/feeds", (req, res) => {
+  const userId = req.session.user_id; // modify to go into SQL database
+  const user = users[userId];
 
+  if (!user) {
+    res.status(401);
+    res.redirect("/");
+  }
+  res.render("feeds", templateVars);
+});
 
+app.get("/resource-builder", (req, res) => {
+  const userId = req.session.user_id;
+  const user = users[userId]; // modify to go into SQL database
+  if (!user) {
+    return res.redirect("/");
+  }
+  // const templateVars = {
+  //   user
+  // };
+  res.render("resource-builder" /*, templateVars */);
+});
+
+app.post("/resource-builder", (req, res) => {
+  // receive value for title, URL, description.
+  // create a query that inserts values for title url description into table resources
+  //receive value for categories
+  //create a query that inserts values for categories into categories table
+  //update feeds table with new resource post
+  // redirect to feeds
+  res.redirect("/feeds");
+});
+
+app.get("/my-resources", (req, res) => {
+  const userId = req.session.user_id; //modify to go into SQL database
+  const user = users[userId];
+
+  if (!user) {
+    res.status(401);
+    res.redirect("/");
+  }
+  // const userDB = urlsForUser(userId, urlDatabase);
+  // const templateVars = {
+  //   user,
+  //   urls: userDB
+  // };
+
+  res.render("my-resources", templateVars);
+});
+
+// app.post("/my-resources", (req, res) => {
+
+// });
+
+app.get("/profile", (req, res) => {
+  // get the cookie value associated to the user id (username)
+  const userId = req.session.user_id;
+  const user = users[userId]; // modify to go into SQL database
+  if (!user) {
+    return res.redirect("/");
+  }
+  const templateVars = {
+    user
+  };
+  res.render('profile', templateVars);
+});
+
+app.post("/profile", (req, res) => {
+  // get the cookie value associated to the user id (username)
+  let username = req.body.username;
+  // create a query to update username
+  res.redirect('/profile')
+  //popup that says "profile updated"
+});
 
