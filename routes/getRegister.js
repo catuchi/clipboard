449fdb81931570144a82bff3c7ccf0088f478510
Chibu <atuchibueze@gmail.com>
2022-05-10 12:55:59 -0400

    create login, register and homepage

--- routes/getRegister.js
+++ routes/getRegister.js
@@ -3,6 +3,7 @@ const router  = express.Router();
 
 module.exports = (database) => {
   router.get("/register", (req, res) => {
+    return res.render("register");
     const userId = req.session.userId;
     if (!userId) {
       res.render('register');
