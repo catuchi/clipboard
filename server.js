dd01433b282eaf3461e95d34cc2dd2226455227e
Nathan Bain <nate.bain9@gmail.com>
2022-05-11 02:18:50 -0400

    added dependencies

--- server.js
+++ server.js
@@ -8,7 +8,31 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 
-// PG database client/connection setup
+
+
+
+
+
+
+
+//        in case we need extra dependencies!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+const bodyParser = require("body-parser");
+const cookieSession = require('cookie-session');
+const bcryptjs = require('bcryptjs');
+const generateRandomString = () => {
+  return Math.random().toString(36).substring(6);
+};
+
+
+
+
+
+
+
+
+
+
 const { Pool } = require("pg");
 const dbParams = require("./lib/db.js");
 const db = new Pool(dbParams);
@@ -32,6 +56,10 @@ app.use(
 );
 
 app.use(express.static("public"));
+
+
+
+
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
@@ -94,6 +122,112 @@ app.get("/login", (req, res) => {
     res.redirect("/login");
   }
 });
+
+
+
+
+
+
+
+
+
+
+//trying to add functions from tiny app to recreate the user/password db !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+const getUserByEmail = (email, database) => {
+  return Object.values(database).find(user => user.email === email);
+};
+
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+const addUser = (email, password) => {
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const id = generateRandomString();
+  users[id] = {
+    id,
+    email,
+    password: hashedPassword
+  };
+  return id;
+};
+
+
+// in case we need to parse middleware
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: [' '],
+  })
+);
+
+app.post("/login", (req,res) => {
+  const { email, password } = req.body;
+  const user = getUserByEmail(email, users);
+  if (!user) {
+    let templateVars = {
+      status: 401,
+      message: "Can't find email.... where is it?",
+      user: users[req.session.user_id]
+    };
+    res.status(401);
+    res.render("urls_error", templateVars);
+  } else if (!bcryptjs.compareSync(password, user.password)) {
+    let templateVars = {
+      status: 401,
+      message: "BRRR Password Incorrecto",
+      user: users[req.session.user_id]
+    };
+    res.status(401);
+    res.render("urls_error", templateVars);
+  } else {
+    req.session.user_id = user.id;
+    res.redirect("/urls");
+  }
+});
+
+
+
+app.post("/register", (req, res) => {
+  const { email, password } = req.body;
+  let user = {email: 'undefined'};
+  if (!email || !password) {
+    let templateVars = {
+      status: 400,
+      message: "so... your email and/or password is missing...",
+      user
+    };
+    res.status(401);
+    res.render("urls_error", templateVars);
+    ("so... your email and/or password is missing...");
+  } else if (getUserByEmail(email, users)) {
+    let templateVars = {
+      status: 409,
+      message: "Uh... that email is already registered sooo.....",
+      user: users[req.session.user_id]
+    };
+    res.status(409);
+    res.render("urls_error", templateVars);
+  } else {
+    const user_id = addUser(email, password, users);
+    req.session.user_id = user_id;
+    res.redirect("/urls");
+  }
+});
+
+
+
 
 
 
