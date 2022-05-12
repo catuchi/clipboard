d3f7a9884379b6e836da2f0481ff51ab74f9e824
Nathan Bain <nate.bain9@gmail.com>
2022-05-12 11:49:31 -0400

    resolve conflict for merge

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
-// const test = require("./routes/test");
+//const test = require("./routes/test");
 const homepage = require("./routes/homepage");
 const getLogin = require("./routes/getLogin");
 const postLogin = require("./routes/postLogin");
